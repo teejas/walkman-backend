96a9e5a1a4e4ca9b9a74c5834935ec06872f3745
Tejas Siripurapu <tejassiripurapu@tejass-mbp.lan>
2022-07-24 15:07:29 -0700

    update allowed_hosts

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "juko.tejas.wtf"]
+ALLOWED_HOSTS = ["localhost", "juko.tejas.wtf", "10.98.0.0/16"]
 
 CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'https://juko.tejas.wtf']
 
